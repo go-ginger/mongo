4622f7a1a2e80f7aef662ca82d1ca020a5fd2f0f
saeed <saeid.kamyabi71@gmail.com>
2020-01-27 17:17:44 +0330

    set BaseModel.UpdatedAt nullable

--- base.go
+++ base.go
@@ -11,7 +11,7 @@ type BaseModel struct {
 
 	ID        *primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
 	CreatedAt time.Time           `bson:"created_at,omitempty" json:"created_at,omitempty" mongo:"insert_only"`
-	UpdatedAt time.Time           `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
+	UpdatedAt *time.Time           `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
 	DeletedAt *time.Time          `bson:"deleted_at,omitempty" json:"deleted_at,omitempty"`
 }
 
